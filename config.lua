10d26dc5053036aa0751ab0eae37b7a5a261a428
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-03-21 15:38:45 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,9 +3,3 @@ Config = {}
 Config.Unit = "KMH" -- MPH/KMH
 Config.Style = "glass" -- clean/glass
 Config.UpdateTick = 100 -- Aktualisierungsrate // refresh rate
-
-Config.Colors = {
-    rpm = "#ffffff",
-    fuel = "#3cff9c",
-    lowFuel = "#ff4444"
-}
