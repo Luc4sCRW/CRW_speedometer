3385338b24eca66eec921f8d9c8571fd5baca7d1
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-03-21 15:44:13 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,5 @@ Config = {}
 
 Config.Unit = "KMH" -- MPH/KMH
 Config.Style = "glass" -- clean/glass
+Config.Shape = "squared" -- rounded/squared
 Config.UpdateTick = 100 -- Aktualisierungsrate // refresh rate
