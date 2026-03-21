8efce0ad7906c4c4b53646fd969b1221ecbad19c
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-03-21 13:58:06 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,16 +1,17 @@
+----------------------------------------------------- v1.0.0 -----------------------------------------------------
 local unit = Config.Unit or "KMH"
 local tick = Config.UpdateTick or 100
 
-------------------- STYLE HOLEN
+-- Stil holen. // get style.
 CreateThread(function()
-    Wait(1000) -- Etwas mehr Zeit für das NUI
+    Wait(1000)
     SendNUIMessage({
         action = "setStyle",
         style = Config.Style or "glass"
     })
 end)
 
------------------------------------------
+-- Haupt Schleife // main loop
 Citizen.CreateThread(function()
     while true do
         local sleep = 500
@@ -21,19 +22,17 @@ Citizen.CreateThread(function()
             sleep = tick
             local speed = GetEntitySpeed(veh)
             local rpm = GetVehicleCurrentRpm(veh)
-                -- Motor aus? RPM auf 0 setzen
             if not GetIsVehicleEngineRunning(veh) then
                 rpm = 0
             end
                 
-            -- Hier die Berechnung für KMH (3.6) oder MPH (2.236)
+            -- Berechnung für KMH (3.6) oder MPH (2.236). // calculation for KMH (3.6) or MPH (2.236).
             local finalSpeed = (unit == "KMH" and speed * 3.6 or speed * 2.236936)
-            
+                
             SendNUIMessage({
                 type = "updateVehicleHud",
                 show = true,
                 speed = finalSpeed,
-                --rpm = GetVehicleCurrentRpm(veh),
                 rpm = rpm,
                 fuel = GetVehicleFuelLevel(veh),
                 gear = GetVehicleCurrentGear(veh)
@@ -46,10 +45,7 @@ Citizen.CreateThread(function()
     end
 end)
 
-
-
---------------- MOTOR AUS AN --------------
-
+-- Tastenbelegung für Motor an/aus. // keybind for engine toggle.
 RegisterCommand("toggleEngine", function()
     local ped = PlayerPedId()
     local veh = GetVehiclePedIsIn(ped, false)
@@ -60,10 +56,9 @@ RegisterCommand("toggleEngine", function()
     end
 end, false)
 
-RegisterKeyMapping("toggleEngine", "Motor an/aus", "keyboard", "M")
+RegisterKeyMapping("toggleEngine", "Engine on/off", "keyboard", "M")
 
---------------- STEERING --------------
-
+-- Einschlagwinkel des Lenkrads speichern, wenn Motor aus. // save the steering wheel angle when the engine is off.
 Citizen.CreateThread(function()
     local angle = 0.0
     local speed = 0.0
