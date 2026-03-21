eb3953c26da8b8b741fc0613987aedf17a0247d4
Luc4s_CRW <luc4sbuisnesscrw@gmail.com>
2026-03-21 13:33:26 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.Unit = "KMH" -- "KMH" oder "MPH"
+Config.Style = "glass" -- clean/glass
+Config.UpdateTick = 100 -- Intervall in ms (100ms ist flüssig und performant)
+
+Config.Colors = {
+    rpm = "#ffffff",
+    fuel = "#3cff9c",
+    lowFuel = "#ff4444"
+}
